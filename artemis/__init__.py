d82ef5fb1f5f9a611d337c49637a4adb9b4fe7a3
JiFish <joe@jifish.co.uk>
2021-01-24 23:43:20 +0000

    Better FPS counter

--- artemis/__init__.py
+++ artemis/__init__.py
@@ -269,7 +269,7 @@ def tick():
     __INSTRUCTIONS_COUNT = 0
 
     if fps_flag:
-        print ("{:.0f} fps   ".format(1/max(0.00000000001,time.time()-__ticktime)), end='\r')
+        print ("{:.2f} fps   ".format(__CLOCK.get_fps()), end='\r')
         __ticktime = time.time()
 
     __CLOCK.tick(30)
